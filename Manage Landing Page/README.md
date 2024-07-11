6935f73c0a734da4e70f05f9d516b48efd76c241
kaushik-2318 <kaushikverma321@gmail.com>
2024-07-11 10:51:42 +0530

    Manage Landing Page Done

---
+++ Manage Landing Page/README.md
@@ -0,0 +1,64 @@
+# Frontend Mentor - Manage landing page solution
+
+This is a solution to the [Manage landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/manage-landing-page-SLXqC6P5). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See all testimonials in a horizontal slider
+- Receive an error message when the newsletter sign up `form` is submitted if:
+  - The `input` field is empty
+  - The email address is not formatted correctly
+
+### Screenshot
+
+![](./src/screenshot.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- React
+
+
+### What I learned
+
+Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+
+
+## Author
+
+- Website - [Kaushik Verma](https://portfolio-six-teal-24.vercel.app/)
+- Frontend Mentor - [@kaushik-2318](https://www.frontendmentor.io/profile/kaushik-2318)
+- Twitter - [@SilentK68296830](https://x.com/SilentK68296830)
+
+
+## Acknowledgments
+
+This is where you can give a hat tip to anyone who helped you out on this project. Perhaps you worked in a team or got some inspiration from someone else's solution. This is the perfect place to give them some credit.
---
+++ Manage Landing Page/src/App.tsx
@@ -0,0 +1,185 @@
+import { useState } from 'react'
+
+import illustrationintro from './assets/illustration-intro.svg'
+import anisha from './assets/avatar-anisha.png'
+import ali from './assets/avatar-ali.png'
+import richard from './assets/avatar-richard.png'
+import shanai from './assets/avatar-shanai.png'
+
+
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/pagination';
+import 'swiper/css';
+import { Pagination } from 'swiper/modules';
+
+
+
+import './App.css'
+
+function App() {
+
+  const [isopen, setIsopen] = useState(false);
+
+
+  const peoples = [{
+    name: "Anisha Li",
+    text: " “Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”",
+    img: anisha
+  }, {
+    name: "Ali Bravo",
+    text: "“We have been able to cancel so many other subscriptions since using Manage.There is no more cross- channel confusion and everyone is much more focused.”",
+    img: ali
+  }, {
+    name: "Richard Watts",
+    text: "“Manage allows us to provide structure and process. It keeps us organized and focused.I can’t stop recommending them to everyone I talk to!”",
+    img: richard
+  }, {
+    name: "Shanai Gough",
+    text: "“Their software allows us to track, manage and collaborate on our projects from anywhere.It keeps the whole team in-sync without being intrusive.”",
+    img: shanai
+  }]
+
+  return (
+    <>
+      <div>
+        <NavBar panel={isopen} setpanel={setIsopen} />
+        <main className="background md:h-screen flex justify-center items-center md:p-28 py-10 px-12 gap-10 overflow-hidden flex-col-reverse md:flex-row">
+          <div className="md:w-1/2 h-full flex md:justify-end md:items-start justify-center items-center flex-col">
+            <div className="md:text-6xl text-4xl font-bold text-[var(--DarkBlue)] text-center md:text-left">
+              Bring everyone together to build better products.
+            </div>
+            <div className="py-10 text-justify text-[var(--DarkGrayishBlue)] md:w-[60%]">
+              Manage makes it simple for software teams to plan day-to-day
+              tasks while keeping the larger team goals in view.
+            </div>
+            <div>
+              <button className={`rounded-full text-[var(--VeryLightGray)] hover:bg-[var(--clr-accent-300)] bg-[var(--BrightRed)] outline-none px-5 py-3 text-white shadow-2xl w-full`}>
+                Get Started
+              </button >
+            </div>
+          </div>
+
+          <div className="md:w-1/2 w-full flex justify-center items-center md:h-full">
+            <img className="md:w-[80%]" src={illustrationintro} alt="Illustration Intro" />
+          </div>
+        </main>
+
+
+        <section className="backgroundabout flex md:justify-start items-center justify-center md:px-28 py-28 gap-10 md:h-screen md:flex-row flex-col">
+          <div className="md:w-1/2 w-full h-full flex justify-center items-left flex-col px-10">
+            <div className="md:text-6xl text-3xl font-bold text-[var(--DarkBlue)] md:text-left text-center">
+              What’s different about Manage?
+            </div>
+            <div className="py-10 text-justify text-[var(--DarkGrayishBlue)] md:w-[60%] ">
+              Manage provides all the functionality your team needs, without
+              the complexity. Our software is tailor-made for modern digital
+              product teams.
+            </div>
+          </div>
+          <div className="md:w-1/2 flex justify-center items-center gap-10 h-full flex-col pl-5 text-[var(--DarkGrayishBlue)]">
+            <div>
+              <div className="flex justify-start items-center flex-row md:gap-8 gap-2 rounded-l-full bg-[var(--VeryPaleRed)] md:bg-transparent mb-5">
+                <div className="w-20">
+                  <button className={`rounded-full text-[var(--VeryLightGray)] bg-[var(--BrightRed)] outline-none px-5 py-3 text-white shadow-2xl w-full`}>
+                    01
+                  </button >
+                </div>
+                <div className="text-[var(--DarkBlue)] font-bold">Track company-wide progress</div>
+              </div>
+              <p className="md:pl-28 text-justify pr-10">
+                See how your day-to-day tasks fit into the wider vision. Go from
+                tracking progress at the milestone level all the way done to the
+                smallest of details. Never lose sight of the bigger picture again.
+              </p>
+            </div>
+
+            <section>
+              <div className="flex justify-start items-center flex-row md:gap-8 gap-2 rounded-l-full bg-[var(--VeryPaleRed)] md:bg-transparent mb-5">
+                <div className="w-20">
+                  <button className={`rounded-full text-[var(--VeryLightGray)] bg-[var(--BrightRed)] outline-none px-5 py-3 text-white shadow-2xl w-full`}>
+                    02
+                  </button >
+                </div>
+                <div className="text-[var(--DarkBlue)] font-bold">Advanced built-in reports</div>
+              </div>
+              <p className="md:pl-28 text-justify pr-10">
+                Set internal delivery estimates and track progress toward company
+                goals. Our customisable dashboard helps you build out the reports
+                you need to keep key stakeholders informed.
+              </p>
+            </section>
+            <div>
+              <div className="flex justify-start items-center flex-row md:gap-8 gap-2 rounded-l-full bg-[var(--VeryPaleRed)] md:bg-transparent mb-5">
+                <div className="w-20">
+                  <button className={`rounded-full text-[var(--VeryLightGray)] bg-[var(--BrightRed)] outline-none px-5 py-3 text-white shadow-2xl w-full`}>
+                    03
+                  </button >
+                </div>
+                <div className="text-[var(--DarkBlue)] font-bold">Everything you need in one place</div>
+              </div>
+              <p className="md:pl-28 text-justify pr-10">
+                Stop jumping from one service to another to communicate, store files,
+                track tasks and share documents. Manage offers an all-in-one team
+                productivity solution.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section className="flex justify-center items-center flex-col gap-28 z-[-4]">
+          <div className="md:text-5xl text-4xl font-bold text-[var(--DarkBlue)] text-center">
+            What they’ve said
+          </div>
+
+            <Swiper slidesPerView={3} spaceBetween={30} pagination={{ clickable: true, }} modules={[Pagination]} className="mySwiper">
+              {peoples.map((people, index) => (
+                <SwiperSlide className='flex justify-center items-center flex-col min-w-96' key={index}>
+                  <div>
+                    <img className='w-14 h-14' src={people.img} alt="Profile Picture" />
+                  </div>
+                  <div className='font-bold text-[var(--DarkBlue)]'>
+                    {people.name}
+                  </div>
+                  <div className='text-center text-[var(--DarkGrayishBlue)] text-sm'>
+                    {people.text}
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+
+
+          <div>
+            <button className={`rounded-full text-[var(--VeryLightGray)] hover:bg-[var(--clr-accent-300)] bg-[var(--BrightRed)] outline-none px-5 py-3 text-white shadow-2xl w-full`}>
+              Get Started
+            </button >
+          </div>
+        </section >
+
+        <section className="cta md:h-60 bg-[var(--BrightRed)] md:gap-80 flex md:justify-around items-center mt-20 justify-center md:flex-row flex-col md:p-0 p-10 gap-10 h-96">
+          <div className='text-white font-bold text-4xl md:w-1/3 text-center'>
+            Simplify how your team works today.
+          </div>
+          <div>
+            <button className={`rounded-full text-[var(--BrightRed)] bg-[var(--VeryLightGray)] outline-none px-5 py-3 shadow-2xl w-full font-bold`}>
+              Get Started
+            </button >
+          </div>
+        </section>
+
+
+        <Footer />
+
+
+        <div className='attribution'>
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+          Coded by <a href="https://portfolio-six-teal-24.vercel.app/">Kaushik Verma</a>.
+        </div>
+      </div >
+    </>
+  )
+}
+
+export default App
